ab59dadfe678fb3c37572232688d5056310efacd
Kevin Riquelme <kriquelme10@gmail.com>
2026-02-25 11:51:14 -0300

    file google search console v3

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://tudominio.com'
+  const baseUrl = 'https://thermolectrica.com'
 
   return [
     {
